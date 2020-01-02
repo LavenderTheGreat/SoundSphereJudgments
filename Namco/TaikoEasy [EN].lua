06f5d78e8a1b63ae83f3cbde8b1791be10085c3d
Caleb Yarnold <34808998+LavenderTheGreat@users.noreply.github.com>
2020-01-02 22:17:26 +0000

    Create TaikoEasy [EN].lua

---
+++ Namco/TaikoEasy [EN].lua
@@ -0,0 +1,57 @@
+local Score = require("sphere.screen.gameplay.CloudburstEngine.Score")
+
+local CustomScore = Score:new()
+
+CustomScore.passEdge = 0.108441665649414
+CustomScore.missEdge = 0.125125
+
+CustomScore.timegates = {
+	{
+		time = 0.0417083358764648,
+		name = "GOOD"
+	},
+	{
+		time = 0.108441665649414,
+		name = "OK"
+	},
+	{
+		time = 0.125125,
+		name = "BAD"
+	}
+}
+
+Score.grades = {
+	{
+		time = 0.012,
+		name = "SS"
+	},
+	{
+		time = 0.032,
+		name = "S"
+	},
+	{
+		time = 0.040,
+		name = "A"
+	},
+	{
+		time = 0.056,
+		name = "B"
+	},
+	{
+		time = 0.072,
+		name = "C"
+	},
+	{
+		time = 0.092,
+		name = "D"
+	},
+	{
+		time = 0.120,
+		name = "E"
+	},
+	{
+		name = "F"
+	},
+}
+
+return CustomScore
