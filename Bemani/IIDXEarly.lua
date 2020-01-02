432443b3dda2b31b6735c293cc352f868ba11d40
Caleb Yarnold <34808998+LavenderTheGreat@users.noreply.github.com>
2020-01-02 21:47:14 +0000

    Add IIDX Early windows
    Based on https://twitter.com/voxandra/status/666790242985369608?lang=en .

---
+++ Bemani/IIDXEarly.lua
@@ -0,0 +1,67 @@
+local Score = require("sphere.screen.gameplay.CloudburstEngine.Score")
+
+local CustomScore = Score:new()
+
+CustomScore.passEdge = 0.1333
+CustomScore.missEdge = 0.1666
+
+CustomScore.timegates = {
+	{
+		time = 0.0166,
+		name = "PGREAT"
+	},
+	{
+		time = 0.0500,
+		name = "GREAT",
+		nameLate = "GREAT -",
+		nameEarly = "+ GREAT"
+	},
+	{
+		time = 0.1333,
+		name = "GOOD",
+		nameLate = "GOOD -",
+		nameEarly = "+ GOOD"
+	},
+	{
+		time = 0.1666,
+		name = "BAD",
+		nameLate = "BAD -",
+		nameEarly = "+ BAD"
+	}
+}
+
+Score.grades = {
+	{
+		time = 0.012,
+		name = "SS"
+	},
+	{
+		time = 0.032,
+		name = "S"
+	},
+	{
+		time = 0.040,
+		name = "A"
+	},
+	{
+		time = 0.056,
+		name = "B"
+	},
+	{
+		time = 0.072,
+		name = "C"
+	},
+	{
+		time = 0.092,
+		name = "D"
+	},
+	{
+		time = 0.120,
+		name = "E"
+	},
+	{
+		name = "F"
+	},
+}
+
+return CustomScore
